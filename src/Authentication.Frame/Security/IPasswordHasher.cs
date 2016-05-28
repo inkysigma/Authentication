6fb9b64d36cf8ad282148b231a4db8a9a1828ea7
Justin Ley <inkysigma@outlook.com>
2016-05-28 14:34:51 -0700

    Added some security frames.

---
+++ src/Authentication.Frame/Security/IPasswordHasher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Authentication.Frame.Security
+{
+    public interface IPasswordHasher<TPassword>
+    {
+        Task<TPassword> HashPassword(string password, byte[] salt, CancellationToken cancellationToken);
+    }
+}
---
+++ src/Authentication.Frame/Security/ISecureRandomProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace Authentication.Frame.Security
+{
+    public interface ISecureRandomProvider
+    {
+        Task<byte[]> GenerateRandomAsync(CancellationToken token);
+    }
+}
