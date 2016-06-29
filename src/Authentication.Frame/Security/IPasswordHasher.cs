ad69a86e753a4347ae68487d0c4264470259e2d1
Justin Ley <inkysigma@outlook.com>
2016-06-28 21:53:51 -0700

    Added verification for password hashing.

--- src/Authentication.Frame/Security/IPasswordHasher.cs
+++ src/Authentication.Frame/Security/IPasswordHasher.cs
@@ -6,5 +6,7 @@ namespace Authentication.Frame.Security
     public interface IPasswordHasher
     {
         Task<byte[]> HashPassword(string password, byte[] salt, CancellationToken cancellationToken);
+
+        Task<byte[]> VerifyPassword(string password, byte[] salt, byte[] verify, CancellationToken cancellationToken);
     }
 }
