80837625508e604d4d4a74eb973e91633b5d07c0
Justin Ley <inkysigma@outlook.com>
2016-07-06 14:57:33 -0700

    Updated the email client to send the generated token rather than the hash.

--- src/Authentication.Frame/UserManager.Create.cs
+++ src/Authentication.Frame/UserManager.Create.cs
@@ -104,7 +104,7 @@ namespace Authentication.Frame
             await EmailConfiguration.AccountVerificationTemplate.LoadAsync(new
             {
                 Email = email,
-                Token = token
+                Token = id
             });
             await EmailConfiguration.EmailProvider.Email(EmailConfiguration.AccountVerificationTemplate, email, cancellationToken);
 
--- src/Authentication.Frame/UserManager.Update.cs
+++ src/Authentication.Frame/UserManager.Update.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using System.Threading;
 using Authentication.Frame.Configuration;
 using Authentication.Frame.Result;
+using Authentication.Frame.Security;
 
 namespace Authentication.Frame
 {
@@ -23,7 +24,7 @@ namespace Authentication.Frame
                     Validation = qResult.Validation
                 };
             var qUser = qResult.Result;
-            var id = Convert.ToBase64String(await SecurityConfiguration.RandomProvider.GenerateRandomAsync(cancellationToken));
+            var id = Guid.NewGuid().ToString();
             var generate = Convert.ToBase64String(SecurityConfiguration.TokenProvider.CreateToken(user, id, Security.TokenField.Password));
             var result = await TokenStore.CreateTokenAsync(qUser, generate, cancellationToken);   
             if (!result.Succeeded || result.RowsModified != 1)
@@ -43,7 +44,7 @@ namespace Authentication.Frame
             await EmailConfiguration.PasswordChangeTemplate.LoadAsync(new
             {
                 Email = email,
-                Token = generate
+                Token = id
             });
             await EmailConfiguration.EmailProvider.Email(EmailConfiguration.PasswordChangeTemplate, email, cancellationToken);
 
@@ -82,6 +83,17 @@ namespace Authentication.Frame
 
             await Commit(cancellationToken, StoreTypes.PasswordStore);
             return AuthenticationResult.Success();
+        }
+
+        public async Task<AuthenticationResult<bool>> ValidateUpdateToken(TUser user, string updateToken, TokenField field, CancellationToken cancellationToken)
+        {
+            if (user == null)
+                throw new ArgumentNullException(nameof(user));
+            if (string.IsNullOrEmpty(updateToken))
+                throw new ArgumentNullException(nameof(updateToken));
+
+            await Commit(cancellationToken, StoreTypes.TokenStore);
+            return AuthenticationResult<bool>.Success(true);
         }
     }
 }
